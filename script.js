5dd5a9561a57152ae068d7f8cf0036b4d2200194
sarahjshaw <sarahj108.25@gmail.com>
2020-12-03 21:36:45 -0500

    third commit, removed extra code

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 function divide(num1, num2) {
   num1 = Number(document.getElementById("firstNum").value);
   num2 = Number(document.getElementById("secNum").value);
-  console.log(firstNum);
-  console.log(secNum);
   answer = num1 / num2
   document.getElementById("answer").value = answer
 }
