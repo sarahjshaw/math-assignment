356a6c30fc5f23865d044074a4ff2e8c1edb647a
qelder <qelder5779@yahoo.com>
2020-12-04 00:44:28 -0500

    2nd Commit

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 <<<<<<< HEAD
-function subtract(num1, num2){
-    let box1=Number(document.getElementById("num1").value);
-    let box2=Number(document.getElementById("num2").value);
-    let box3=Number(document.getElementById("result"));
+function subtract(num1, num2) {
+    num1 = Number(document.getElementById("numOne").value);
+    num2 = Number(document.getElementById("numTwo").value);
+    result = num1 - num2
+    document.getElementById("result").value = result
+  }
+    
+      
+    
+     
     
 
-    let result = box1 - box2;
-    box3.innerText=result;
+  
     
-}
+
 
 =======
 function getAddition(firstNum, secondNum){
