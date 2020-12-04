2867c5cb9f9606a31d9fccdabfd9f0bee9f7e371
Mario <m.vongphachant@live.com>
2020-12-04 15:55:19 -0500

    final commit

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const firstInput = document.getElementById('first-number');
-const secondInput = document.getElementById('second-number');
-const total = document.getElementById('total');
+
 const button = document.getElementById('button')
 
 
 button.addEventListener('click', () => { 
+    const firstInput = document.getElementById('first-number');
+    const secondInput = document.getElementById('second-number');
+    const total = document.getElementById('total');
     const sum = firstInput.value * secondInput.value
     total.value = sum;
 })
